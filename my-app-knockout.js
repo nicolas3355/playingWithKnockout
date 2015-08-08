76e36572ef5de822225e86a370a3c35e9773c227
nicolas <nicolaselhaddad.nh@gmail.com>
2015-08-08 18:51:13 +0300

    added src

---
+++ my-app-knockout.js
@@ -0,0 +1,62 @@
+var catsData =[
+    {
+      name:"cat",
+      src:"cat.jpeg",
+      clicks:0,
+      nicknames:['nick1','hu']
+
+    },
+    {
+      name:"cat2",
+      src:"cat2.jpeg",
+      clicks:0,
+      nicknames:['nick2','koko']
+    },
+    {
+      name:"cat3",
+      src:"cat3.jpeg",
+      clicks:0,
+      nicknames:['nick3','jiji']
+    }
+  ];
+var cat = function(data){
+
+  this.name=ko.observable(data.name);
+  this.src=ko.observable(data.src);
+  this.clicks=ko.observable(data.clicks);
+  this.nicknames=ko.observableArray(data.nicknames);
+
+  this.catLevel=ko.computed(function(){
+    switch (true) {
+    case (this.clicks()<10):
+      return "infant";
+    case (this.clicks()<20):
+      return "baby";
+    case (this.clicks()<30):
+      return "child";
+    case(this.clicks()<40):
+      return "teen";
+    default:
+      return "lost count wtf stop clicking";
+    }
+  },this);
+};
+var viewModel = function(){
+  var self=this;
+  this.cats=ko.observableArray();
+
+  catsData.forEach(function(catData){
+    self.cats.push(new cat(catData));
+  });
+
+  this.currentCat=ko.observable(self.cats()[0]);
+
+  this.incrementCounter=function(){
+    self.currentCat().clicks(self.currentCat().clicks()+1);
+  }
+  this.changeToCurrent=function(){
+    self.currentCat(this);
+  }
+};
+
+ko.applyBindings(new viewModel());
